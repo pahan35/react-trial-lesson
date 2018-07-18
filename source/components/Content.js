96c504ba49c7733f23526c01ed7d8630eb0d4503
Pavlo Zhukov <pahazhukov@gmail.com>
2018-07-18 22:17:56 +0300

    Final state

---
+++ source/components/Content.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Profile from "./Profile";
+import Photos from "./Photos";
+
+export default class Content extends React.Component {
+    render () {
+        return (
+            <div className = 'content'>
+                <Profile />
+                <Photos />
+            </div>
+        );
+    }
+}
